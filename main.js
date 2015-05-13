89d0c2478fab6340b7e8f6d87aa626fa2a649b46
Yurii <yuskornyakov@gmail.com>
2015-05-13 21:24:25 +0300

    Calculator functions

---
+++ main.js
@@ -0,0 +1,23 @@
+/*
+* Реализовать калькулятор с помощью функций
+* */
+
+function add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
+
+function divide(firstNumber, secondNumber) {
+    return firstNumber / secondNumber;
+}
+
+function diff(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+}
+
+function modulo(firstNumber, secondNumber) {
+    return firstNumber % secondNumber;
+}
