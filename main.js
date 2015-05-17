e622107455e391b14905018d48a68261ffbc5a51
Yurii <yuskornyakov@gmail.com>
2015-05-17 19:00:05 +0300

    Calculator using prototype 2

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ console.log(calc.divide(5, 3));
 console.log(calc.diff(5, 3));
 console.log(calc.modulo(5, 3));*/
 
-function Calculator() {
+/*function Calculator() {
     return this;
 }
 
@@ -72,6 +72,39 @@ Calculator.prototype.diff = function (firstNumber, secondNumber) {
 Calculator.prototype.modulo = function (firstNumber, secondNumber) {
     return firstNumber % secondNumber;
 };
+
+var calc = new Calculator();
+
+console.log(calc.add(5, 3));
+console.log(calc.multiply(5, 3));
+console.log(calc.divide(5, 3));
+console.log(calc.diff(5, 3));
+console.log(calc.modulo(5, 3));*/
+
+function add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
+function divide(firstNumber, secondNumber) {
+    return firstNumber / secondNumber;
+}
+function diff(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+}
+function modulo(firstNumber, secondNumber) {
+    return firstNumber % secondNumber;
+}
+
+function Calculator() {
+    return this;
+}
+Calculator.prototype.add = add;
+Calculator.prototype.multiply = multiply;
+Calculator.prototype.divide = divide;
+Calculator.prototype.diff = diff;
+Calculator.prototype.modulo = modulo;
 
 var calc = new Calculator();
 
